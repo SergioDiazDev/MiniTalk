f836423f219230d83f3331770be780310ce8e488
sdiaz-ru <sdiaz-ru@student.42malaga.com>
2023-03-14 16:03:45 +0100

    Problemas con la string en binario

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: sdiaz-ru <sdiaz-ru@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/13 15:18:20 by sdiaz-ru          #+#    #+#             */
-/*   Updated: 2023/03/13 16:17:00 by sdiaz-ru         ###   ########.fr       */
+/*   Updated: 2023/03/14 16:02:34 by sdiaz-ru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 #include <stdlib.h>
 #include "ft_printf/ft_printf.h"
 
+void	respuesta(int x);
 long	ft_atoi_pid(char *str);
 char	*ft_decabin(int dec, char *bin);
 
@@ -25,6 +26,7 @@ int	main(int argc, char **argv)
 	int		pid_client;
 	char	*bin;
 	int		i;
+	int		j;
 	char	*aux;
 
 	bin = malloc(sizeof(char) * 9);
@@ -37,16 +39,29 @@ int	main(int argc, char **argv)
 	pid_server = ft_atoi_pid(argv[1]);
 	pid_client = getpid();
 	aux = ft_itoa(pid_client);
+	//ft_printf("Pid Cliente: %d", pid_client);
 	i = -1;
 	while (aux[++i])
 	{
 		ft_decabin(aux[i], bin);
-		ft_printf("\n");
+		j = -1;
+		//ft_printf("Bin = (%d)", bin[i]);
+		while (++j <= 8)
+		{
+			usleep(1000);
+			if (bin[j])
+				kill(pid_server, SIGUSR1);
+			else
+				kill(pid_server, SIGUSR2);
+		}
 	}
-	ft_printf("Pid del server: %d\n", pid_server);
-	ft_printf("Pid del cliente: %d\n", pid_client);
-	kill(pid_server, SIGUSR1);
 	return (0);
+}
+
+void	respuesta(int x)
+{
+	(void) x;
+	ft_printf("Servidor a la espera :)\n");
 }
 
 long	ft_atoi_pid(char *str)
@@ -58,7 +73,7 @@ long	ft_atoi_pid(char *str)
 	num = 0;
 	while (str[i])
 	{
-		num += (str[i] - '0');
+		num += (str[i]);
 		num *= 10;
 		i++;
 	}
@@ -79,7 +94,8 @@ char	*ft_decabin(int dec, char *bin)
 			aux = (dec / bit);
 		else
 			aux = 0;
-		bin[i] = dec % bit;
+		bin[i] = (dec % bit) - '0';
+		ft_printf("Bin[%d] = %c/n",i, bin[i]);
 		if (aux && dec)
 			dec -= bit;
 		if (bit)
--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: sdiaz-ru <sdiaz-ru@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 15:01:14 by sdiaz-ru          #+#    #+#             */
-/*   Updated: 2023/03/13 15:23:22 by sdiaz-ru         ###   ########.fr       */
+/*   Updated: 2023/03/14 15:40:44 by sdiaz-ru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,17 +17,65 @@
 #include "ft_printf/ft_printf.h"
 
 void	respuesta(int x);
+void	respuesta2(int x);
+int		binadec(char *bin);
 
 int	main(void)
 {
+	char *bin;
+	int	pib_client;
+	int	i;
+
+	i  = 0;
+	bin = malloc(sizeof(char) * 9);
+	bin[8] = 0;
 	ft_printf("PID Padre: %d\n", getpid());
-	while (42)
+	while (i <= 8)
 	{
-		signal(SIGUSR1, respuesta);
+		signal(SIGUSR2, respuesta2);
+		if (!signal(SIGUSR1, respuesta))
+		{
+			bin[i] = 0;
+			i++;
+		}
+		else if (!signal(SIGUSR2, respuesta2))
+		{
+			bin[i] = 1;
+			i++;
+		}
+		printf("\n%d\n", getpid());
 	}
+	pib_client = binadec(bin);
+	ft_printf("Pid Client: %s\n", bin);
+	kill(pib_client, SIGUSR2);
 }
 
 void	respuesta(int x)
 {
-	ft_printf("PID Respuesta: %d, %d\n", getpid(), x);
+	(void)x;
+	ft_printf("PID Respuesta: 0\n");
+}
+
+void	respuesta2(int x)
+{
+	(void)x;
+	ft_printf("PID Respuesta: 1\n");
+}
+
+int	binadec(char *bin)
+{
+	int i;
+	int bit;
+	int dec;
+
+	bit = 128;
+	dec = 0;
+	i = -1;
+	while (++i <=8)
+	{
+		if (bin[i])
+			dec += bit;
+		bit /= 2; 
+	}
+	return (dec);
 }
