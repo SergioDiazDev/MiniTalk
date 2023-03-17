537c1e5f59f695c11c0960593fd5fd72b346781f
sdiaz-ru <sdiaz-ru@student.42malaga.com>
2023-03-17 13:23:50 +0100

    Conseguida conexion entre servidor cliente, ahora voy a borrarlo todo y a intentarlo con la estructura que pasa el pid como informacion

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: sdiaz-ru <sdiaz-ru@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 15:01:14 by sdiaz-ru          #+#    #+#             */
-/*   Updated: 2023/03/14 15:40:44 by sdiaz-ru         ###   ########.fr       */
+/*   Updated: 2023/03/17 13:19:08 by sdiaz-ru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,63 +19,79 @@
 void	respuesta(int x);
 void	respuesta2(int x);
 int		binadec(char *bin);
-
+int 	flag = 2;
 int	main(void)
 {
 	char *bin;
 	int	pib_client;
 	int	i;
 
+	signal(SIGUSR1, respuesta);
+	signal(SIGUSR2, respuesta2);
 	i  = 0;
 	bin = malloc(sizeof(char) * 9);
 	bin[8] = 0;
 	ft_printf("PID Padre: %d\n", getpid());
-	while (i <= 8)
+	while (i < 40)
 	{
-		signal(SIGUSR2, respuesta2);
-		if (!signal(SIGUSR1, respuesta))
+		if (flag == 0)
 		{
-			bin[i] = 0;
+			bin[i] = '0';
 			i++;
+			flag = 2;
 		}
-		else if (!signal(SIGUSR2, respuesta2))
+		else if (flag == 1)
 		{
-			bin[i] = 1;
+			bin[i] = '1';
 			i++;
+			flag = 2;
 		}
-		printf("\n%d\n", getpid());
 	}
 	pib_client = binadec(bin);
+	ft_printf("Pid Client: %d\n", pib_client);
 	ft_printf("Pid Client: %s\n", bin);
-	kill(pib_client, SIGUSR2);
+	kill(pib_client, SIGUSR1);
 }
 
 void	respuesta(int x)
 {
 	(void)x;
 	ft_printf("PID Respuesta: 0\n");
+	flag = 0;
 }
 
 void	respuesta2(int x)
 {
 	(void)x;
 	ft_printf("PID Respuesta: 1\n");
+	flag = 1;
 }
 
 int	binadec(char *bin)
 {
 	int i;
+	int j = 0;
 	int bit;
 	int dec;
+	int total = 0; 
 
 	bit = 128;
 	dec = 0;
 	i = -1;
-	while (++i <=8)
+	while (++i < 40)
 	{
-		if (bin[i])
+		if (bin[i] == '1')
 			dec += bit;
-		bit /= 2; 
+		bit /= 2;	
+		j++;
+		if(!(j % 8))
+		{
+			total *= 10;
+			total += dec;
+			bit = 128;
+			dec = 0;
+		}
+
 	}
-	return (dec);
+	return (total);
 }
