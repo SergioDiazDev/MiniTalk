ab713b471bd8f00bc4eb1ef0284aeadc2609d3c5
sdiaz-ru <sdiaz-ru@student.42malaga.com>
2023-03-19 13:54:16 +0100

    [PENDIENTE]Crear variable que se desactive al recibir SIGUSR2 y continue con SIGUSR

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 /*   By: sdiaz-ru <sdiaz-ru@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/23 15:01:14 by sdiaz-ru          #+#    #+#             */
-/*   Updated: 2023/03/19 13:35:54 by sdiaz-ru         ###   ########.fr       */
+/*   Updated: 2023/03/19 13:56:08 by sdiaz-ru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,20 @@ void	signal_exit(int x, siginfo_t *info, void *param)
 	(void) param;
 	ft_printf("PID Respuesta: %d\n", x);
 	ft_printf("PID: %d\n", info->si_pid);
+	//Crear variable que se desactive al recibir SIGUSR2 y continue con SIGUSR
+	while ()
+	{
+		if (info->si_signo == SIGUSR1)
+		{
+			printf("0");
+		}
+		else if (info->si_signo == SIGUSR2)
+		{
+			printf("1");
+		}
+	}
 	kill(info->si_pid, SIGUSR1);
+	
 	exit(0);
 }
 
