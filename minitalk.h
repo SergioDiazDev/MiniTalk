4d01821083409a0a402be7bd28b520b239154db0
sdiaz-ru <SergioDiazDev@gmail.com>
2023-03-20 13:31:11 +0100

    Eliminadas librerias no usadas

--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: sdiaz-ru <sdiaz-ru@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/18 11:15:00 by sdiaz-ru          #+#    #+#             */
-/*   Updated: 2023/03/20 13:14:12 by sdiaz-ru         ###   ########.fr       */
+/*   Updated: 2023/03/20 13:28:56 by sdiaz-ru         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,6 @@
 
 # include <unistd.h>
 # include <signal.h>
-# include <stdio.h>
-# include <stdlib.h>
 # include "ft_printf/ft_printf.h"
 
 //Server
